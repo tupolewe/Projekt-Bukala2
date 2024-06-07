a0fe733283f56d364b7a5b7cc08d7f8d902f9426
jvdash <judamateusz1@gmail.com>
2024-06-07 10:36:16 +0200

    Prototyp narrative text UI

---
+++ Assets/UI/UI Scripts/NarrativeUIScript.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.UI;
+using UnityEngine;
+
+public class NarrativeUIScript : MonoBehaviour
+{
+
+    public GameObject MainCamera;
+    public GameObject narrativeUI;
+    public GameObject narrativeText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        narrativeUI.transform.localScale = new Vector3(0, 29, 29);
+        narrativeText.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.LookRotation(transform.position - MainCamera.transform.position);
+        ScaleCheck();
+    }
+
+
+    void ScaleCheck()
+    {
+        if (narrativeUI.transform.localScale == new Vector3(29, 28.878f, 28.878f))
+        {
+            narrativeText.SetActive(true);
+        }
+        else
+        {
+            narrativeText.SetActive(false);
+        }
+    } 
+}
+
+
