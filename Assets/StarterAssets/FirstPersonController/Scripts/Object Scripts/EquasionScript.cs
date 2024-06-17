a8c4f92cf708cc5bd0a33a5c1a8d903715a9a7b5
jvdash <judamateusz1@gmail.com>
2024-06-17 20:46:54 +0200

    Update EquasionScript.cs

--- Assets/StarterAssets/FirstPersonController/Scripts/Object Scripts/EquasionScript.cs
+++ Assets/StarterAssets/FirstPersonController/Scripts/Object Scripts/EquasionScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EquasionScript : MonoBehaviour
+public class EquasionScript : MonoBehaviour, Interactable 
 {
     // Start is called before the first frame update
     void Start()
@@ -14,5 +14,11 @@ public class EquasionScript : MonoBehaviour
     void Update()
     {
         
+    }
+
+
+    public void Interact()
+    {
+
     }
 }
